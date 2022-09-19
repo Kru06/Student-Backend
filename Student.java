dee1ccad93d989b7e45b72f8814f42fb1aed2a7e
Kru06 <89855611+Kru06@users.noreply.github.com>
2022-09-19 13:52:39 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,52 @@
+package com.example.studentbackend.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+
+public class Student {
+    @Id
+
+    private Integer id;
+
+    private String name;
+
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+}
